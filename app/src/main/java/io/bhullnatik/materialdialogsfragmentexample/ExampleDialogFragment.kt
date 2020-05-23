5b9890ebe8c96ac8738cba223269b7cee5050b03
Jimmy Mayoukou <bhullnatik@gmail.com>
2020-05-23 13:40:47 +0800

    Use setView in created Dialog to display dialog fragment

--- app/src/main/java/io/bhullnatik/materialdialogsfragmentexample/ExampleDialogFragment.kt
+++ app/src/main/java/io/bhullnatik/materialdialogsfragmentexample/ExampleDialogFragment.kt
@@ -2,25 +2,16 @@ package io.bhullnatik.materialdialogsfragmentexample
 
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ExampleDialogFragment : DialogFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.dialog_example, container, false)
-    }
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return MaterialAlertDialogBuilder(requireContext()).create()
+        return MaterialAlertDialogBuilder(requireContext()).setView(R.layout.dialog_example)
+            .create()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
