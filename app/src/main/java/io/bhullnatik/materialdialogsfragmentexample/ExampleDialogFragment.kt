addb9c2db8b9b57fe04748b0010165a427583204
Jimmy Mayoukou <bhullnatik@gmail.com>
2020-05-23 13:38:37 +0800

    Override onCreateDialog in Dialog Fragment, but it doesn't work :(

--- app/src/main/java/io/bhullnatik/materialdialogsfragmentexample/ExampleDialogFragment.kt
+++ app/src/main/java/io/bhullnatik/materialdialogsfragmentexample/ExampleDialogFragment.kt
@@ -1,11 +1,13 @@
 package io.bhullnatik.materialdialogsfragmentexample
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ExampleDialogFragment : DialogFragment() {
 
@@ -15,6 +17,10 @@ class ExampleDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.dialog_example, container, false)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return MaterialAlertDialogBuilder(requireContext()).create()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
